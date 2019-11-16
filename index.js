b8811699f43a7f68166745bdbaa44b87ddab82f6
Marc Bachmann <marc.brookman@gmail.com>
2019-11-16 15:43:28 +0100

    feat: Promise and async function support

--- index.js
+++ index.js
@@ -3,16 +3,51 @@ module.exports = actionMiddlewareFactory
 function actionMiddlewareFactory (config) {
   if (!config.action) return
 
-  var action = config.action
-  return function executeActionMiddleware (cmd, callback) {
+  const action = config.action
+  if (action.length >= 2) return executeCallbackAction
+  else return executeAsyncAction
+
+  function executeCallbackAction (cmd, callback) {
+    let err, res
     try {
-      action.call(this, cmd, next)
+      let sameTick
+      action.call(this, cmd, function next (_err, _res) {
+        if (sameTick === false) {
+          if (_err) return callback(_err)
+          return callback(null, _res)
+        } else {
+          sameTick = true
+          err = _err
+          res = _res
+        }
+      })
+      if (sameTick === undefined) {
+        sameTick = false
+        return
+      }
+    } catch (_err) {
+      err = _err
+    }
+
+    if (err) return callback(err)
+    return callback(null, res)
+  }
+
+  function executeAsyncAction (cmd, callback) {
+    let res
+    try {
+      res = action.call(this, cmd)
     } catch (err) {
       return callback(err)
     }
 
-    function next (err, res) {
-      setImmediate(callback, err, res)
+    if (res && res.then) {
+      res.then(
+        (res) => setImmediate(callback, null, res),
+        (err) => setImmediate(callback, err)
+      )
+    } else {
+      return callback(null, res)
     }
   }
 }
